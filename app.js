215b384d93b9476eebb8c7c49e223595d20e6c8e
TomerMoveo <61459202+TomerMoveo@users.noreply.github.com>
2020-03-01 13:34:54 +0200

    change at the end /api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const app = express();
+const app_1 = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -31,9 +32,10 @@ app.use('/restuarants', allRestuarants);
 app.use('/popular', popularRes);
 app.use('/signature', signatureMeals);
 app.use('/resbychef', resByChef);
+app_1.use('/api', app);
 
 // * My port * //
-app.listen (port ,err => {
+app_1.listen (port ,err => {
     if (err) {
         return console.log('EROOR:', err);
     }
